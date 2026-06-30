246aee7e50246421a5cc4088a9f0472cf42840b7
griffith <xjulianbarragan@gmail.com>
2026-06-30 22:26:21 +0200

    initiated templates for implementation

---
+++ transformer.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class MultiHeadAttention(nn.Module):
+    pass
+
+class EncoderLayer(nn.Module):
+    pass
+
+class DecoderLayer(nn.Module):
+    pass
+
+class FeedForward(nn.Module):
+    """Position wise feed forward network"""
+    pass
+
+class PositionalEncoding(nn.Module):
+    pass
+
+class Transformer(nn.Module):
+    pass
