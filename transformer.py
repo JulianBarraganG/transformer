13f51e8514f9893a8ec669647ffd11a64b4e24e8
JulianBarraganG <jbar@basecph.com>
2026-07-23 16:41:08 +0200

    implemented scaled dot prod

--- transformer.py
+++ transformer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import softmax
 import torch.optim as optim
 
 
@@ -17,10 +18,26 @@ class MultiHeadAttention(nn.Module):
         self.d_k = d_model // h
         self.d_v = d_v if d_v is not None else self.d_k
         self.W_O = nn.Linear((h * self.d_v), d_model)
+        self.W_Q = nn.Linear(d_model, self.d_k)
+        self.W_K = nn.Linear(d_model, self.d_k)
+        self.W_V = nn.Linear(d_model, self.d_v)
 
-    def scaled_dot_product_attention(self, Q, K, V):
+    def scaled_dot_product_attention(
+        self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor
+    ) -> torch.Tensor:
         """Scaled dot product attention."""
-    pass
+        assert Q.shape[1] == K.shape[1] == self.d_k,(
+            f"Expected Q and K to have {self.d_k} i.e. d_k columns"
+        )
+        assert V.shape[1] == self.d_v, f"Expected V to have {self.d_v} i.e. d_v columns"
+        scaled_dot_prods = softmax(torch.matmul(Q, torch.t(K)) / self.d_k)
+
+        return torch.matmul(scaled_dot_prods, V)
+
+    def multi_head(
+        self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor
+    ) -> torch.Tensor:
+        pass
 
 class EncoderLayer(nn.Module):
     pass
